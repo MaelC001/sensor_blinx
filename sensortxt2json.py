a4c0bfb5e5f0581c74b5a2c217a5940eabc6820a
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-06-18 09:39:17 -0400

    update list sensor

---
+++ sensortxt2json.py
@@ -0,0 +1,54 @@
+a = ["Autre : Rotary Encoder + parametre : Digital in (https://wiki.keyestudio.com/Ks0013_keyestudio_Rotary_Encoder_Module)","Autre : Reed Switch + led : digital in (https://wiki.keyestudio.com/Ks0038_keyestudio_Reed_Switch_Module)","Telecommand : New Infrared IR Wireless Remote Control : digital in (https://wiki.keyestudio.com/Ks0088_New_Infrared_IR_Wireless_Remote_Control_Module_Kits_for_Arduino)","Autre : 3-channel Line Tracking : digital in x3 (https://wiki.keyestudio.com/KS0453_Keyestudio_3-channel_Line_Tracking_Module)","Autre : AD KEY Button Module : digital in (https://wiki.keyestudio.com/KS0466_Keyestudio_AD_KEY_Button_Module(Black_and_Eco-friendly))","Autre : Knock Sensor : digital in (https://wiki.keyestudio.com/Ks0024_keyestudio_Knock_Sensor_Module)","Autre : Digital Tilt : digital in (https://wiki.keyestudio.com/Ks0025_keyestudio_Digital_Tilt_Sensor)","Autre : Capacitive Touch : digital in (https://wiki.keyestudio.com/Ks0031_keyestudio_Capacitive_Touch_Sensor)","Autre : Flame Sensor : digital in (https://wiki.keyestudio.com/Ks0036_keyestudio_Flame_Sensor)","Autre : Vibration Sensor : digital in (https://wiki.keyestudio.com/Ks0037_keyestudio_Vibration_Sensor)","Autre : Infrared Obstacle Avoidance Sensor : digital in (https://wiki.keyestudio.com/Ks0051_keyestudio_Infrared_Obstacle_Avoidance_Sensor)","Autre : Line Tracking Sensor : digital in (https://wiki.keyestudio.com/Ks0050_keyestudio_Line_Tracking_Sensor)","Autre : PIR Motion Sensor : digital in (https://wiki.keyestudio.com/Ks0052_keyestudio_PIR_Motion_Sensor)","Autre : Non-contact Liquid Level Sensor (XKC-Y25-T12V) : digital in (https://wiki.keyestudio.com/KS0346_Keyestudio_Non-contact_Liquid_Level_Sensor(Black_and_Friendly))","Autre : Temperature and Humidity Sensor (DHT11) : digital in (https://wiki.keyestudio.com/Ks0034_keyestudio_DHT11_Temperature_and_Humidity_Sensor) (https://github.com/micropython/micropython/blob/master/drivers/dht/dht.py)","Autre : Digital Push Button : digital in (https://wiki.keyestudio.com/Ks0029_keyestudio_Digital_Push_Button)","Autre : Load Cell Pressure Sensor(HX711) : analog in x2 (https://wiki.keyestudio.com/Ks0167_keyestudio_HX711_Load_Cell_Pressure_Sensor_Module) (https://github.com/SergeyPiskunov/micropython-hx711)","Autre : Slide Potentiometer Module : analog in (https://wiki.keyestudio.com/Ks0279_keyestudio_Slide_Potentiometer_Module_for_Arduino)","Autre : Collision Sensor : analog in (https://wiki.keyestudio.com/Ks0021_keyestudio_Collision_Sensor)","Autre : Pulse Rate Monitor : analog in (https://wiki.keyestudio.com/Ks0015_keyestudio_Pulse_Rate_Monitor)","temp : Linear Temperature (LM35) : anlog in (https://wiki.keyestudio.com/Ks0022_keyestudio_LM35_Linear_Temperature_Sensor)","lumiere : Photocell : analog in (https://wiki.keyestudio.com/Ks0028_keyestudio_Photocell_Sensor)","temp : Analog Temperature : analog in (https://wiki.keyestudio.com/Ks0033_keyestudio_Analog_Temperature_Sensor)","Autre : Microphone Sound Sensor with Potentiometer : analog in (https://wiki.keyestudio.com/KS0035_Microphone_Sound_Sensor_with_Potentiometer)","Autre : Analog Gas Sensor : analog in (https://wiki.keyestudio.com/Ks0040_keyestudio_Analog_Gas_Sensor)","Autre : Analog Alcohol Sensor : analog in (https://wiki.keyestudio.com/Ks0041_keyestudio_Analog_Alcohol_Sensor)","Autre : Water Sensor : analog in (https://wiki.keyestudio.com/Ks0048_keyestudio_Water_Sensor)","Autre : Soil Humidity Sensor : analog in (https://wiki.keyestudio.com/Ks0049_keyestudio_Soil_Humidity_Sensor)","Autre : Ambient Light Sensor (TEMT6000) : analog in (https://wiki.keyestudio.com/Ks0098_keyestudio_TEMT6000_Ambient_Light_Sensor)","Autre : Current Sensor (ACS712-30A) : analog in (https://wiki.keyestudio.com/Ks0185_keyestudio_ACS712-30A_Current_Sensor)","Autre : Current Sensor (ACS712-20A) : analog in (https://wiki.keyestudio.com/Ks0186_keyestudio_ACS712-20A_Current_Sensor)","Autre : Current Sensor (ACS712-5A) : analog in (https://wiki.keyestudio.com/Ks0187_keyestudio_ACS712-5A_Current_Sensor)","Autre : Steam Sensor : analog in (https://wiki.keyestudio.com/Ks0203_keyestudio_Steam_Sensor)","Autre : Ultraviolet Sensor (GUVA-S12SD 3528) : analog in (https://wiki.keyestudio.com/Ks0204_keyestudio_GUVA-S12SD_3528_Ultraviolet_Sensor)","Autre : Analog Piezoelectric Ceramic Vibration Sensor : analog in (https://wiki.keyestudio.com/Ks0272_keyestudio_Analog_Piezoelectric_Ceramic_Vibration_Sensor)","Autre : Current Sensor Current Detection Sensor : analog in (https://wiki.keyestudio.com/Ks0274_keyestudio_Current_Sensor_Current_Detection_Sensor)","Autre : Voltage Detection Module Voltage Sensor : analog in (https://wiki.keyestudio.com/Ks0275_keyestudio_Voltage_Detection_Module_Voltage_Sensor)","Autre : Thin-film Pressure Sensor : analog in (https://wiki.keyestudio.com/Ks0309_Keyestudio_Thin-film_Pressure_Sensor_(Black_and_Eco-friendly))","Autre : Pulse Sensor (XD-58C) : analog ou (https://wiki.keyestudio.com/Ks0171_keyestudio_XD-58C_Pulse_Sensor_Module)","Autre : Ultraviolet Sensor (GY-ML8511) : analog ou (https://wiki.keyestudio.com/Ks0273_keyestudio_GY-ML8511_Ultraviolet_Sensor_Module)","Autre : Vibration Motor Module : analog ou (https://wiki.keyestudio.com/KS0450_Keyestudio_Vibration_Motor_Module_(Black_and_Eco-friendly))","Led : 10mm Highlight Full-color LED RGB : analog ou x3 (https://wiki.keyestudio.com/KS0459_Keyestudio_10mm_Highlight_Full-color_LED_RGB_Module)"]
+
+for i in a:
+    i = i.replace('https:', 'https..').replace('_(', '___')
+    b = i.split(':')
+    t = {
+            "name": "",
+            "categorie": "",
+            "sensor": "",
+            "url": "",
+            "image": "",
+            "url_code": "",
+            "url_code_example": "",
+            "liaison": "and",
+            "info": []
+        }
+    for ii in range(len(b)):
+        if ii == 0:
+            t['categorie'] = b[ii][:-1]
+        elif ii == 1:
+            if '(' in b[ii]:
+                aq = b[ii].split('(')
+                t['name'] = aq[0][1:-1]
+                t['sensor'] = aq[1][:-2]
+            else:
+                t['name'] = b[ii][1:-1]
+        elif ii == 2:
+            aq = b[ii].split('(')
+            info = aq[0][:-1]
+            infoI = info.split(',')
+            for iii in infoI:
+                ttt = {
+                    "typePort": "",
+                    "in_out": "",
+                    "number": ""
+                }
+                infoII = iii.split(' ')
+                infoII.pop(0)
+                ttt['typePort'] = infoII[0].replace('digital', 'Digital').replace('analog', 'Analog')
+                ttt['in_out'] = infoII[1]
+                if len(infoII) == 3:
+                    ttt['number'] = str(infoII[2][1:])
+                else:
+                    ttt['number'] = str(1)
+
+                t['info'].append(ttt)
+
+            if len(aq) == 3:
+                t['url'] = aq[1][:-2].replace('https..', 'https:').replace('___', '_(')
+                t['url_code_example'] = aq[2][:-1].replace('https..', 'https:').replace('___', '_(')
+            else:
+                t['url'] = aq[1][:-1].replace('https..', 'https:').replace('___', '_(')
+    print(t,',')
+
