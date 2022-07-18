03f16ae847e9a78443baed32ca74ef1fdda58d37
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-07-18 16:47:36 -0400

    add display

---
+++ display.py
@@ -0,0 +1,11 @@
+class create_display():
+    pass
+
+info = {
+    'name' : ['led', 'buzzer_digi', 'buzzer_passive', 'relay_5v', '3w_led', 'white_led', 'red_led', 'green_led', 'yellow_led', 'blue_led', 'sfe_reed_switch', 'reed_switch', 'tilt', 'capacitive_touch', 'flame_sensor', '801s', 'infrared_obstacle', 'line_tracking', 'pir', 'xkc_y25_t12v', 'dht11', 'push_button', 'collision_sensor'], 
+    'info' : {}, 
+    'channels' : {}, 
+    'functions' : {
+        'create' : create_display,
+    }, 
+}
