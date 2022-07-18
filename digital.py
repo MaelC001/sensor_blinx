87ca20d4a8cdce98289ad112dd4f2a53cbd4d1b7
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-07-18 14:14:02 -0400

    update list

--- digital.py
+++ digital.py
@@ -9,7 +9,7 @@ def immediate(i2c, info):
     return ';'.join(result)
 
 info = {
-    'name' : ['led', 'buzzer_digi', 'buzzer_passive', 'relay_5v', '3w_led', 'white_led', 'red_led', 'green_led', 'yellow_led', 'blue_led'],
+    'name' : ['led', 'buzzer_digi', 'buzzer_passive', 'relay_5v', '3w_led', 'white_led', 'red_led', 'green_led', 'yellow_led', 'blue_led', 'SFE_Reed_Switch', 'Reed_Switch'],
     'info' : {
         #'freq' : '', # analog
         #'addr' : '', # I2C
