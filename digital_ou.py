42a538f3f7bfb264938fd7dd71e4a193ae8b0adb
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-07-18 11:49:55 -0400

    example py sensor

--- digital_ou.py
+++ digital_ou.py
@@ -1,5 +1,32 @@
 from machine import Pin
 
-def write(pin, value):
-    # pin = Pin(pin, mode=Pin.OUT)
-    return pin.value(value)
+def immediate(i2c, info):
+    result = []
+    for pin_info in info:
+        pin = pin_info['pin']
+        t = Pin(pin, Pin.OUT)
+        result.append(b'\x01' if t() else b'\x00')
+    return ';'.join(result)
+
+info = {
+    'name' : [''],
+    'info' : {
+        #'freq' : '', # analog
+        #'addr' : '', # I2C
+        #'byteReceive' : '', # I2C
+        #'codeSend' : '', # I2C
+    },
+    'channels' : {
+        '0' : {
+            'waiting' : 0,
+            'functionsId' : {
+                #'byte' : '', # analog + I2C : lambda x,y,z : x
+                #'data' : '', # analog + I2C : lambda x:x
+            },
+            'functions' : {
+                'immediate' : immediate,
+                #'create' : '', # display
+            },
+        },
+    },
+}
