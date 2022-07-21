c84686ddbd2cc956c9998fe083054d546f6e63c2
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-07-20 22:50:41 -0400

    update sensor

--- digital.py
+++ digital.py
@@ -9,7 +9,7 @@ def immediate(i2c, info):
     return ';'.join(result)
 
 info = {
-    'name' : ['led', 'buzzer_digi', 'buzzer_passive', 'relay_5v', '3w_led', 'white_led', 'red_led', 'green_led', 'yellow_led', 'blue_led', 'sfe_reed_switch', 'reed_switch', 'tilt', 'capacitive_touch', 'flame_sensor', '801s', 'infrared_obstacle', 'line_tracking', 'pir', 'xkc_y25_t12v', 'dht11', 'push_button', 'collision_sensor'], 
+    'name' : ['cs100a', 'relay2', 'a4988', 'led', 'buzzer_digi', 'buzzer_passive', 'relay_5v', '3w_led', 'white_led', 'red_led', 'green_led', 'yellow_led', 'blue_led', 'sfe_reed_switch', 'reed_switch', 'tilt', 'capacitive_touch', 'flame_sensor', '801s', 'infrared_obstacle', 'line_tracking', 'pir', 'xkc_y25_t12v', 'dht11', 'push_button', 'collision_sensor'], 
     'info' : {
         #'freq' : '', # analog
         #'addr' : '', # I2C
