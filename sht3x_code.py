00b11241534753efc435047c5ab324f34e715313
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-08-24 10:55:10 -0400

    Delete sht3x_code.py

--- sht3x_code.py
+++
@@ -1,8 +0,0 @@
-import time
-from machine import I2C, Pin
-i2c = I2C(sda = Pin(4), scl = Pin(5))
-addr = 0x70
-i2c.writeto(addr, b'\x7C\xA2')
-time.sleep_ms(15)
-sht = i2c.readfrom(addr, 6)
-print(sht)
