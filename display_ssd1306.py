a595c0f6e5bc4a23283347159f23998f22f1fb50
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-08-30 16:09:39 -0400

    verify type args in display

--- display_ssd1306.py
+++ display_ssd1306.py
@@ -10,26 +10,34 @@ class create_display():
             y = value[1]
         self.display = ssd1306.SSD1306_I2C(x, y, i2c)
         self.__register = {
-            'power_off': self.display.poweroff,
-            'power_on': self.display.poweron,
-            'contrast': self.display.contrast,
-            'invert': self.display.invert,
-            'rotate': self.display.rotate,
-            'show': self.display.show,
-            'fill': self.display.fill,
-            'pixel': self.display.pixel,
-            'line': self.display.line,
-            'vline': self.display.vline,
-            'hline': self.display.hline,
-            'rect': self.display.rect,
-            'fill_rect': self.display.fill_rect,
-            'text': self.display.text,
-            'scroll': self.display.scroll,
-            'blit': self.display.blit,
+            'power_off': {'func':self.display.poweroff, 'type': [[]]},
+            'power_on': {'func':self.display.poweron, 'type': [[]]},
+            'contrast': {'func':self.display.contrast, 'type': [[int]]},
+            'invert': {'func':self.display.invert, 'type': [[int]]},
+            'rotate': {'func':self.display.rotate, 'type': [[bool]]},
+            'show': {'func':self.display.show, 'type': [[]]},
+            'fill': {'func':self.display.fill, 'type': [[int]]},
+            'pixel': {'func':self.display.pixel, 'type': [[int, int], [int, int, int]]},
+            'line': {'func':self.display.line, 'type': [[int, int, int, int, int]]},
+            'vline': {'func':self.display.vline, 'type': [[int, int, int, int, int]]},
+            'hline': {'func':self.display.hline, 'type': [[int, int, int, int, int]]},
+            'rect': {'func':self.display.rect, 'type': [[int, int, int, int, int]]},
+            'fill_rect': {'func':self.display.fill_rect, 'type': [[int, int, int, int, int]]},
+            'text': {'func':self.display.text, 'type': [[str, int, int, int]]},
+            'scroll': {'func':self.display.scroll, 'type': [[int, int]]},
+            #'blit': {'func':self.display.blit, 'type': [..., int, int, int]},
         }
     def function(self, func_name, *array_value):
         if func_name in self.__register:
-            return self.__register[func_name](*array_value)
+            temp = self.__register[func_name]
+            s = []
+            for i in range(len(temp['type'])):
+                if len(array_value) in len(temp['type'][i]):
+                    array_value_format = []
+                    for y in range(len(array_value)):
+                        array_value_format.append(temp['type'][y](array_value[i][y]))
+                    return temp['func'](*array_value_format)
+            raise KeyError('Not the good amount of arguments.')
         else:
             raise KeyError('Function name for the display don\'t exist.')
 
